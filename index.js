686bba4c947d3ac9c3bf83c93d2de72235c38109
Rohan Palankar <rohanpalankar8@gmail.com>
2022-11-23 00:00:49 +0530

    modify files

--- index.js
+++ index.js
@@ -3,11 +3,56 @@ const add = document.querySelector('.add');
 const taskList = document.querySelector('.task_list')
 const caution = document.querySelector('.caution');
 
+
+let todoItems = [];
+
 add.addEventListener("click",() => {
     updateList();
 });
 
+todoItems = JSON.parse(localStorage.getItem("todo"));
+renderTodos(todoItems);
+
 function updateList() {
+    if(input.value === ""){
+        caution.style.display = 'inline-block';
+        return;
+    }
+    
+    caution.style.display = 'none';
+
+    const task = input.value;
+
+    todoItems.push({task});
+    localStorage.setItem("todo", JSON.stringify(todoItems));
+    renderTodos(todoItems);
+
+    input.value = "";
+    
+}
+
+
+function renderTodos(todos) {
+    taskList.innerHTML = "";
+    todos.map(
+        (todo) => {
+            taskList.innerHTML += `<li class="item">${todo.task}<button class="delete">Delete</button></li>`
+        }
+    );
+}
+
+taskList.addEventListener('click',(event) => {
+    console.log(event);
+    if(event.target.classList.contains('delete')){
+        event.target.parentElement.remove();
+    }
+})
+
+
+
+/*
+
+function updateList_1() {
     if(input.value == ""){
         caution.style.display = 'inline-block';
     }
@@ -30,10 +75,6 @@ function updateList() {
     }
 }
 
-taskList.addEventListener('click',(event) => {
-    if(event.target.classList.contains('delete')){
-        event.target.parentElement.remove();
-    }
-})
 
 
+*/
